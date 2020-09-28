0eb5f724a4df2b6ae69d5606383df381fe6adeca
Guy Cherkesky <guy@cherkesky.com>
2020-09-28 17:09:13 -0500

    hardcoded s3put path - working

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,7 @@ config :ex_aws,
   json_codec: Jason,
   access_key_id: {:system, "AWS_ACCESS_KEY_ID"},
   secret_access_key: {:system, "AWS_SECRET_KEY_ID"}
+  # region: {:system, "us-east-2"}
 
 
   # export AWS_ACCESS_KEY_ID="........keyhere..........."
--- lib/s3.ex
+++ lib/s3.ex
@@ -16,8 +16,24 @@ defmodule S3 do
     :world
   end
 
-  def s3ls do
+  def bucketsls do
     ExAws.S3.list_buckets() |> ExAws.request!() |> get_in([:body, :buckets])
   end
+  def objectsls(bucket) do
+    ExAws.S3.list_objects(bucket) |> ExAws.request!() |> get_in([:body, :contents])
+  end
 
+  @spec s3put :: :ok | ExAws.Operation.S3.t()
+  def s3put do
+    image = IO.gets("Please enter the file name & path: ")
+      |> String.trim()
+      |>IO.inspect()
+
+    case File.read("elixir.png") do
+      {:ok, content} -> ExAws.S3.put_object("s3-elixir-s3","images/elixir.png", content)
+                        |> ExAws.request!()
+      {:error, reason} -> IO.puts(reason)
+      end
+      # ExAws.S3.put_object("s3elixirs3", "images/elixir.png", image)
+  end
 end
